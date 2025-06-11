699187064e306d082d3f8b936c84205cdd728cfe
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-06-11 15:42:26 -0300

    Criado a associação da entidades Muito-Para-Um e feito a config do banco H2

--- demo/src/main/java/com/example/demo/entities/Departamento.java
+++ demo/src/main/java/com/example/demo/entities/Departamento.java
@@ -1,4 +1,43 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_departamento")
 public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @OneToMany(mappedBy = "departamento")
+    private List<Pessoa> pessoaList = new ArrayList<>();
+
+    public Departamento() {
+    }
+
+    public Departamento(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Pessoa> getPessoaList() {
+        return pessoaList;
+    }
 }
